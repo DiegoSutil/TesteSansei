5d38d7742b5acf6928e5008416ed82076f1b9f04
DiegoSutil <148872242+DiegoSutil@users.noreply.github.com>
2025-07-08 18:35:50 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth.js";
+import { getFirestore, collection, getDocs, doc, getDoc, setDoc, updateDoc, arrayUnion, arrayRemove } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4-kp4wBq6fz-pG1Rm3VQcq6pO17OEeOI",
+  authDomain: "sansei-d3cf6.firebaseapp.com",
+  projectId: "sansei-d3cf6",
+  storageBucket: "sansei-d3cf6.appspot.com",
+  messagingSenderId: "774111823223",
+  appId: "1:774111823223:web:c03c73c4b89d96244b8d72"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// ... (O resto do script completo está aqui)
+// ...
